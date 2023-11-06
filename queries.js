30d0360ca36178e0dbeb120d4a99a3b13ecc4935
Leonie <leonie@Leonies-MBP-2.fritz.box>
2023-11-06 08:43:09 +0100

    include hybrid search and change filtered search to bm25

--- queries.js
+++ queries.js
@@ -5,45 +5,21 @@ const client = weaviate.client({
     scheme: 'https',
     host: process.env.WEAVIATE_URL,  // Replace with your endpoint
     apiKey: new weaviate.ApiKey(process.env.WEAVIATE_API_KEY),// Replace w/ your Weaviate instance API key  
-    headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY}, 
+    headers: { 'X-OpenAI-Api-Key': process.env.OPENAI_API_KEY },
 });
 
-let num_movies = 40;
+let num_movies = 20;
 
-//Query to fetch filtered results
-async function get_filtered_results(text) {
+
+//Query to fetch keyword results
+async function get_keyword_results(text) {
     let data = await client.graphql
         .get()
         .withClassName('Movies')
+        .withBm25({query: text,
+            properties: ['title^3', 'director', 'genres', 'actors', 'keywords', 'description', 'plot'],
+    })
         .withFields(['title', 'poster_link', 'genres', 'year', 'director', 'movie_id'])
-        .withWhere({
-            operator: 'Or',
-            operands: [{
-                path: ["title"],
-                operator: "Like",
-                valueString: "*" + text + "*"
-            },
-            {
-                path: ["director"],
-                operator: "Like",
-                valueString: "*" + text + "*"
-            },
-            {
-                path: ["genres"],
-                operator: "Like",
-                valueString: "*" + text + "*"
-            },
-            {
-                path: ["keywords"],
-                operator: "Like",
-                valueString: "*" + text + "*"
-            },
-            {
-                path: ["actors"],
-                operator: "Like",
-                valueString: "*" + text + "*"
-            }]
-        })
         .withLimit(num_movies)
         .do()
         .then(info => {
@@ -90,9 +66,44 @@ async function get_semantic_results(text) {
 
 }
 
+//Query to fetch results by sematic searching
+async function get_hybrid_results(text) {
+    if (text.length === 0) {
+        let data = await client.graphql
+            .get()
+            .withClassName('Movies')
+            .withFields(['title', 'poster_link', 'genres', 'year', 'director', 'movie_id'])
+            .withLimit(num_movies)
+            .do()
+            .then(info => {
+                return info
+            })
+            .catch(err => {
+                console.error(err)
+            });
+        return data;
+    } else {
+        let data = await client.graphql
+            .get()
+            .withClassName('Movies')
+            .withFields(['title', 'poster_link', 'genres', 'year', 'director', 'movie_id'])
+            .withHybrid({ query: text, alpha: 0.5 })
+            .withLimit(num_movies)
+            .do()
+            .then(info => {
+                return info
+            })
+            .catch(err => {
+                console.error(err)
+            });
+        return data;
+    }
+
+
+}
+
 //Query to fetch movie details
 async function get_movie_details(id) {
-    console.log(id)
     let data = await client.graphql
         .get()
         .withClassName('Movies')
@@ -118,8 +129,8 @@ async function get_recommended_movies(mov_id) {
         .get()
         .withClassName('Movies')
         .withFields(['title', 'genres', 'year', 'poster_link', 'movie_id'])
-        .withNearObject({id: mov_id})
-        .withLimit(num_movies+1)
+        .withNearObject({ id: mov_id })
+        .withLimit(num_movies + 1)
         .do()
         .then(info => {
             return info;
@@ -127,8 +138,8 @@ async function get_recommended_movies(mov_id) {
         .catch(err => {
             console.error(err)
         });
-    return data
+    return data;
 }
 
 //Exporting these function as they need to be used in index.js
-module.exports = { get_filtered_results, get_semantic_results, get_movie_details, get_recommended_movies }
+module.exports = { get_keyword_results, get_semantic_results, get_hybrid_results, get_movie_details, get_recommended_movies }
